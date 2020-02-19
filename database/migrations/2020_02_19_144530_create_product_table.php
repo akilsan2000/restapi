d845a3d2c615a54aa22e2820ab0be7a24e1004a2
akilsanskanthakumar <akilsan.skanthakumar@lernende.bfo-vs.ch>
2020-02-19 15:59:29 +0100

    Migrations erstellt

---
+++ database/migrations/2020_02_19_144530_create_product_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('availability');
+            $table->double('price');
+            $table->double('special_price');
+            $table->boolean('vegetarian');
+            $table->boolean('vegan');
+            $table->string('image');
+            $table->string('supplier');
+            $table->integer('article_nr');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
